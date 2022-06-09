636777091af3e8a5d77908f31c2d9c37ab4f08e2
vlaii <45049572+we2oxy@users.noreply.github.com>
2022-06-09 20:54:00 +0800

    Update 02_diy_app.sh

--- scripts/02_diy_app.sh
+++ scripts/02_diy_app.sh
@@ -16,16 +16,16 @@ sed -i '/result.encrypt_method/a\                result.fast_open = "1"' root/us
 
 
 # vssr
-svn export https://github.com/jerrykuku/luci-app-vssr/trunk/ /build/immortalwrt/feeds/luci/applications/luci-app-vssr/
-cd /build/immortalwrt/feeds/luci/applications/luci-app-vssr/
-sed -i '/result.encrypt_method/a\        result.fast_open = "1"' root/usr/share/vssr/subscribe.lua
-grep -C 3 result.fast_open root/usr/share/vssr/subscribe.lua
-sed -i 's,ispip.clang.cn/all_cn.txt,cdn.jsdelivr.net/gh/QiuSimons/Chnroute@master/dist/chnroute/chnroute.txt,g' luasrc/controller/vssr.lua
-sed -i 's,ispip.clang.cn/all_cn.txt,cdn.jsdelivr.net/gh/QiuSimons/Chnroute@master/dist/chnroute/chnroute.txt,g' root/usr/share/vssr/update.lua
+#svn export https://github.com/jerrykuku/luci-app-vssr/trunk/ /build/immortalwrt/feeds/luci/applications/luci-app-vssr/
+#cd /build/immortalwrt/feeds/luci/applications/luci-app-vssr/
+#sed -i '/result.encrypt_method/a\        result.fast_open = "1"' root/usr/share/vssr/subscribe.lua
+#grep -C 3 result.fast_open root/usr/share/vssr/subscribe.lua
+#sed -i 's,ispip.clang.cn/all_cn.txt,cdn.jsdelivr.net/gh/QiuSimons/Chnroute@master/dist/chnroute/chnroute.txt,g' luasrc/controller/vssr.lua
+#sed -i 's,ispip.clang.cn/all_cn.txt,cdn.jsdelivr.net/gh/QiuSimons/Chnroute@master/dist/chnroute/chnroute.txt,g' root/usr/share/vssr/update.lua
 
 
 # files
 svn export https://github.com/we2oxy/OpenWrtConfig/trunk/files/etc /build/immortalwrt/files/etc
 cp -R /build/immortalwrt/files/etc/ssrplus/ /build/immortalwrt/files/etc/vssr/
-cp /home/runner/work/Build-Immortalwrt/Build-Immortalwrt/uciconf/vssr /build/immortalwrt/files/etc/config/vssr
+#cp /home/runner/work/Build-Immortalwrt/Build-Immortalwrt/uciconf/vssr /build/immortalwrt/files/etc/config/vssr
 cp /home/runner/work/Build-Immortalwrt/Build-Immortalwrt/uciconf/shadowsocksr /build/immortalwrt/files/etc/config/shadowsocksr
