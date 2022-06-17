e13cd524c2882d98dfdc4325bdedccfb9bea181d
we2oxy <45049572+we2oxy@users.noreply.github.com>
2022-06-18 00:09:29 +0800

    update

--- scripts/source_code/lede-R2S.sh
+++ scripts/source_code/lede-R2S.sh
@@ -13,27 +13,10 @@ rm -rf tmp/
 
 # change uci setting
 sed -i '$d' /build/lede/package/lean/default-settings/files/zzz-default-settings
-
 cat /home/runner/work/Build-Immortalwrt/Build-Immortalwrt/uciconf/R2S/R2S-lede >> /build/lede/package/lean/default-settings/files/zzz-default-settings
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' /build/lede/package/lean/default-settings/files/zzz-default-settings
 echo "####################"
 cat -n /build/lede/package/lean/default-settings/files/zzz-default-settings
 echo "####################"
 
-
-#cat /home/runner/work/Build-Immortalwrt/Build-Immortalwrt/uciconf/R2S/network_dhcp_settings >> /build/lede/package/lean/default-settings/files/zzz-default-settings
-# del default passwd
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' /build/lede/package/lean/default-settings/files/zzz-default-settings
-
-
-# luci-theme-argon
-#rm -rf /build/immortalwrt/feeds/luci/themes/luci-theme-argon
-#svn export https://github.com/jerrykuku/luci-theme-argon/branches/18.06/  /build/immortalwrt/feeds/luci/themes/luci-theme-argon
-
-# change default IP and version flag
-#sed -i 's#-SNAPSHOT##g' /build/immortalwrt/include/version.mk
-#sed -i 's#-SNAPSHOT##g' /build/immortalwrt/package/base-files/image-config.in
 sed -i 's#192.168.1.1#192.168.1.188#' /build/lede/package/base-files/files/bin/config_generate
-
-# add build date
-#sed -i "/Load Average/i\\\t\t<tr><td width="33%"><%:Build Date %></td><td>`date +%F_%T`</td></tr>" /build/immortalwrt/package/emortal/autocore/files/generic/index.htm
-#grep "Build Date" /build/immortalwrt/package/emortal/autocore/files/generic/index.htm
